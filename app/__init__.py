ab528dc31d5ce423a30dfedb10c67b59938a44d3
Lemmy Mwaura <lemminton@gmail.com>
2022-03-07 18:16:56 +0300

    Refactored run.py and init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
-from app.auth import auth
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
-app.register_blueprint(auth, url_prefix='/auth')
+db = SQLAlchemy()
+DB_USER='postgres'
+DB_PASS='adminlemmy'
+
+def create_app():
+    app = Flask(__name__)
+
+    from .views import view
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(view, url_prefix='/')
+
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@localhost/pitchesapp'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    return app
 
 from app import views
 from app import errors
