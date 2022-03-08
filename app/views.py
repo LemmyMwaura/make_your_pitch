2432eb7941d3572e96795f06c4cdb5d9dc54b8f8
Lemmy Mwaura <lemminton@gmail.com>
2022-03-08 17:04:34 +0300

    added login functionality + validations

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template
-from app import app
+from flask import render_template, Blueprint
+from flask_login import current_user,login_required
 
-@app.route('/')
-@app.route('/home')
+view = Blueprint('view',__name__)
+
+@view.route('/')
+@view.route('/home')
 def home():
-    return '<h1>home</h1>'
+    return render_template('index.html', user=current_user)
