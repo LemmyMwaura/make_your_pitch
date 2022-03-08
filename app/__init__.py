32d4c96aa98b06d4cf9bdc5c6ee1c544fa7043d4
Lemmy Mwaura <lemminton@gmail.com>
2022-03-08 19:42:37 +0300

    Installed and configured flask migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 DB_USER='postgres'
 DB_PASS='adminlemmy'
 
@@ -18,9 +20,25 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@localhost/pitchesapp'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
+    migrate.init_app(app,db)
+    create_database(app)
+
+    from .models import User
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
 
 from app import views
 from app import errors
--- app/models.py
+++ app/models.py
@@ -2,17 +2,29 @@ from app import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    users = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.name}'
+
 class User(db.Model, UserMixin):
     __tablename__= 'users'
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(80),)
-    lastname = db.Column(db.String(80),)
-    email = db.Column(db.String(80), unique=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(200))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Posts', backref='poster', lazy='dynamic')
 
-    def __init__(self, firstname, lastname, email, username, password, date_created):
+    def __init__(self, firstname, lastname, email, username, password):
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
@@ -21,3 +33,14 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f'User {self.username}'
+
+class Posts(db.Model):
+    __tablename__='posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    date_posted = db.Column(db.DateTime(timezone=True), default=func.now())
+    poster_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return f'User {self.name}'
